66f5bd6501f50979835393be05888b881f72ec25
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2025-08-21 14:58:21 +0530

    feat: restructure frontend for Electron integration and add routing
    - Updated package.json to include Electron dependencies and scripts for building and running the application.
    - Implemented lazy loading for routes in App.tsx, adding pages for Home, Dashboard, Authenticate, Profile, and Notfound.
    - Added new assets for application icons.
    - Created Electron main process in main.ts, including window management and tray functionality.
    - Added path resolver and utility functions for Electron.
    - Implemented preload script for context bridge.
    - Created database utility using IndexedDB for user management.
    - Added TypeScript definitions for User and Org types.
    - Updated Vite configuration for building the React app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const Authenticate = lazy(() => import("./pages/authenticate"));
+const Notfound = lazy(() => import("./pages/notfound"));
+const Profile = lazy(() => import("./pages/profile"));
+
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="h-full w-full">
+			<Suspense fallback={<div>...</div>}>
+				<Routes>
+					<Route index path="/" element={<Home />} />
+					<Route path="/:orgId/dashboard" element={<Dashboard />} />
+					<Route path="/authenticate" element={<Authenticate />} />
+					<Route path="/profile/:userId?" element={<Profile/>}/>
+					<Route path="*" element={<Notfound />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
