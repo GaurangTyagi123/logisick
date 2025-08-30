d5da9e18ffc13ec463910bd9ccfbe93430b2fe7b
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2025-08-30 20:58:48 +0530

    Add : comments added comments to app.ts, server.ts, types.d.ts, authController.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,39 @@
 import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+import helmet from "helmet"; // For security headers
+import morgan from "morgan"; // Only for development.
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
+// Api ROUTERS
 import authRouter from "./routes/auth.routes";
-import globalErrorController from "./controllers/globalError.controller";
 import passport from "passport";
-import oauthRouter from "./routes/oauth.routes";
 import userRouter from "./routes/user.routes";
 
+// Open Authentication Routes
+import oauthRouter from "./routes/oauth.routes";
+
+// Global Error Handler for requests
+import globalErrorController from "./controllers/globalError.controller";
+
+// Initialize the application
 const app = express();
+
+// Middleware for parsing json in request body
 app.use(express.json());
+
+// Middleware to parse the cookies sent with the request object
 app.use(cookieParser());
+
+// Middleware to initialize Open Authentication
 app.use(passport.initialize());
 
+// Middleware which gives additional information about the requests comming to the server
 app.use(morgan("dev"));
+
+
 app.use(helmet({ hidePoweredBy: true, noSniff: true, xssFilter: true }));
+
+// Middleware to configure response to cross-origin requests
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // app.use(
@@ -29,9 +47,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // 	}
 // );
 
+// Api routes
 app.use("/api/v1/auth", authRouter);
-app.use("/auth", oauthRouter);
 app.use("/api/v1/users", userRouter);
+
+// Open Authentication Routes
+app.use("/auth", oauthRouter);
+
+// Global Error Handler
 app.use(globalErrorController);
 
 export default app;
--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,20 +1,44 @@
+//I UTILS
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import User from '../models/user.model';
-import crypto from 'crypto';
-import { promisify } from 'util';
 import Email from '../utils/sendEmail';
 import { genProfileString } from '../utils/avatarGen';
+import { promisify } from 'util';
 
+import crypto from 'crypto';
+
+//I MODELS
+import User from '../models/user.model';
+
+//I TYPES
+import jwt, { type JwtPayload } from 'jsonwebtoken';
 import type { StringValue } from 'ms';
 
+// @returns json-webtoken with user-id as payload
 const signToken = (id: ObjectId) => {
     const JWTSign = process.env.JWT_SIGN as string;
     const JWTExpire = process.env.JWT_EXPIRE_TIME as StringValue;
     return jwt.sign({ id }, JWTSign, { expiresIn: JWTExpire });
 };
 
+/**
+ *    @objective : To send the generated json-webtoken with the response header
+ *    @param: user(UserType) res(Express Response object) statusCode
+ *    @returns : JSON response ->
+ *           {
+ *                status: "success",
+ *		        data: {
+ *			        user: {
+ *                       _id: stinrg,
+ *                       name: stinrg,
+ *                       email: stinrg,
+ *                       isVerified: user.isVerified,
+ *                       role: stinrg,
+ *                       avatar: stinrg,
+ *                       createdAt: date,
+ *                       updatedAt: datet,
+ *           }
+ */
 const sendNewToken = (
     user: UserType,
     res: ExpressTypes.Response,
@@ -36,22 +60,28 @@ const sendNewToken = (
 
     res.cookie('jwt', token, cookieOptions);
 
-	return res.status(statusCode).json({
-		status: "success",
-		data: {
-			user: {
-				_id: user._id,
-				name: user.name,
-				email: user.email,
-				isVerified: user.isVerified,
-				role: user.role,
-				avatar: user.avatar,
-				createdAt: user.createdAt,
-				updatedAt: user.updatedAt,
-			},
-		},
-	});
+    return res.status(statusCode).json({
+        status: 'success',
+        data: {
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isVerified: user.isVerified,
+                role: user.role,
+                avatar: user.avatar,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+            },
+        },
+    });
 };
+
+/**
+ * @obective To restrict according to the roles
+ * @param roles: admin | manager | staff
+ * @returns NA
+ */
 export const restrictTo = (...roles: string[]) => {
     return (
         req: ExpressTypes.Request,
@@ -63,6 +93,14 @@ export const restrictTo = (...roles: string[]) => {
         return next(new AppError('You are not authorized', 401));
     };
 };
+/**
+ * @params req(UserRequest) res(ExpressRexponse) next(Express Next Function)
+ * @headers jwt(http only cookie)
+ * @approach check if the json-webtoken in the header is correct.
+ *           IF the token is correct then call the next middleware
+ *           ELSE return 401 (unauthorized access)
+ * @sideEffect attaches user data to the request object of the subsequent middlewares
+ */
 export const protect = catchAsync(
     async (
         req: ExpressTypes.UserRequest,
@@ -96,37 +134,47 @@ export const protect = catchAsync(
         return next();
     }
 );
-
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @body email (user email) password (user password)
+ * @return IF (details are correct) send new json-webtoken
+ *         ELSE return status 400
+ */
 export const login = catchAsync(
-	async (
-		req: ExpressTypes.Request,
-		res: ExpressTypes.Response,
-		next: ExpressTypes.NextFn
-	) => {
-		const { email, password } = req.body;
-		if (!email || !password)
-			return next(
-				new AppError("Please provide a valid email and password", 400)
-			);
-		const user = await User.findOne({ email }).select("+password");
+    async (
+        req: ExpressTypes.Request,
+        res: ExpressTypes.Response,
+        next: ExpressTypes.NextFn
+    ) => {
+        const { email, password } = req.body;
+        if (!email || !password)
+            return next(
+                new AppError('Please provide a valid email and password', 400)
+            );
+        const user = await User.findOne({ email }).select('+password');
 
-		if (
-			!user ||
-			!(await user.comparePasswords(password, user.password as string))
-		)
-			return next(new AppError("No such user exists", 401));
+        // comparePasswords compares the password entered by the user and the password stored in the database
+        if (
+            !user ||
+            !(await user.comparePasswords(password, user.password as string))
+        )
+            return next(new AppError('No such user exists', 401));
 
         sendNewToken(user, res, 200);
     }
 );
-
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @approach remove json-webtoken cookie from request header
+ * @return IF (details are correct) send new json-webtoken
+ *         ELSE return status 400
+ */
 export const logout = catchAsync(
     async (
         req: ExpressTypes.UserRequest,
         res: ExpressTypes.Response,
         next: ExpressTypes.NextFn
     ) => {
-        await User.findByIdAndUpdate(req.user?._id);
         const cookieOptions: cookieOptionsType = {
             httpOnly: true,
             expires: new Date(Date.now() + 10),
@@ -138,48 +186,64 @@ export const logout = catchAsync(
     }
 );
 
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @requestHeader json-webtoken
+ * @return IF (json-webtoken is valid) return user data
+ *         ELSE return status:fail with reason for the same
+ */
 export const isLoggedIn = catchAsync(
-	async (
-		req: ExpressTypes.Request,
-		res: ExpressTypes.Response,
-		next: ExpressTypes.NextFn
-	) => {
-		let token: string | undefined;
-		if (req.cookies) token = req.cookies?.jwt;
-		if (!token)
-			return res.status(200).json({
-				status: "fail",
-				isLoggedIn: false,
-				data: {
-					message: "Not logged in",
-				},
-			});
+    async (
+        req: ExpressTypes.Request,
+        res: ExpressTypes.Response,
+        next: ExpressTypes.NextFn
+    ) => {
+        let token: string | undefined;
+        if (req.cookies) token = req.cookies?.jwt;
+        if (!token)
+            return res.status(200).json({
+                status: 'fail',
+                isLoggedIn: false,
+                data: {
+                    message: 'Invalid token',
+                },
+            });
 
-		const verifyAsync = promisify(jwt.verify) as (
-			token: string,
-			secret: string
-		) => Promise<JwtPayload>;
-		const { id, iat: issuedAt } = await verifyAsync(
-			token,
-			process.env.JWT_SIGN as string
-		);
-		const user = await User.findById(id);
-		if (!user || user.passwordUpdatedAfter(issuedAt as number)) {
-			return res.status(200).json({
-				status: "fail",
-				isLoggedIn: false,
-			});
-		}
-		return res.status(200).json({
-			status: "success",
-			isLoggedIn: true,
-			data: {
-				user,
-			},
-		});
-	}
+        const verifyAsync = promisify(jwt.verify) as (
+            token: string,
+            secret: string
+        ) => Promise<JwtPayload>;
+        const { id, iat: issuedAt } = await verifyAsync(
+            token,
+            process.env.JWT_SIGN as string
+        );
+        const user = await User.findById(id);
+        if (!user || user.passwordUpdatedAfter(issuedAt as number)) {
+            return res.status(200).json({
+                status: 'fail',
+                isLoggedIn: false,
+                data: {
+                    message: 'password was updated recently',
+                },
+            });
+        }
+        return res.status(200).json({
+            status: 'success',
+            isLoggedIn: true,
+            data: {
+                user,
+            },
+        });
+    }
 );
 
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @body name(user name) email (user email) password (user password) confirmPassword (password confirmation)
+ * @return IF (details are correct) store user's data in the database and send the new token with the request header
+ *         ELSE IF(details are incomplete) return status:400 (invalid request)
+ *         ELSE return status:500 (internal server error)
+ */
 export const signup = catchAsync(
     async (
         req: ExpressTypes.Request,
@@ -202,6 +266,17 @@ export const signup = catchAsync(
     }
 );
 
+/**
+ * @params req(User Request) res(Express Response) next(Express Next Function)
+ * @preCondition user is logged in
+ * @body OTP or empty
+ * @approach if the body is empty then generate and store the otp in the database and send it to user's email
+ *           else check if the user's otp is same as the otp stored in the database and if it is then update user's isVerified field to true
+ * @return IF (user is verified) send status:200 message: Your email is already verified
+ *         ELSE IF (user's otp is correct) status:200 message email verified successfully
+ *         ELSE return status:400 message:invalid otp
+ * @sideEffect User email is verified
+ */
 export const verifyEmail = catchAsync(
     async (
         req: ExpressTypes.UserRequest,
@@ -219,6 +294,7 @@ export const verifyEmail = catchAsync(
                 },
             });
         else if (isOtpGen && userOtp) {
+            // Find a user which has the same otp and the otp's expire time is greater than the current time
             const user = await User.findOne({
                 otp: userOtp,
                 otpExpireTime: { $gte: Date.now() },
@@ -272,7 +348,13 @@ export const verifyEmail = catchAsync(
         }
     }
 );
-
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @preCondition user is logged in
+ * @body email (user email)
+ * @return IF (email is correct) send reset password url with reset-token to the user's email
+ *         ELSE return status:400 (invalid request)
+ */
 export const forgotPassword = catchAsync(
     async (
         req: ExpressTypes.Request,
@@ -322,6 +404,13 @@ export const forgotPassword = catchAsync(
     }
 );
 
+/**
+ * @params req(Express Request) res(Express Response) next(Express Next Function)
+ * @body prevPassword (user's previous password) password (new password)
+ * @requestParams resetToken
+ * @return IF (details are correct) reset the password and generate new token
+ *         ELSE return status:400 message: invalid token or previous password
+ */
 export const resetPassword = catchAsync(
     async (
         req: ExpressTypes.Request,
@@ -362,6 +451,14 @@ export const resetPassword = catchAsync(
         sendNewToken(user, res, 200);
     }
 );
+
+/**
+ * @params req(User Request) res(Express Response) next(Express Next Function)
+ * @preCondition user is logged in
+ * @body password (new password) confirmPassword (password confirmation)
+ * @return IF (details are correct) update the password and send the updated user data as response
+ *         ELSE return status:400 message:incomplete details
+ */
 export const updatePassword = catchAsync(
     async (
         req: ExpressTypes.UserRequest,
