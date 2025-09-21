de326b06efabba126ba627a5295df10719dc5af8
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2025-09-21 18:26:05 +0530

    FIX : better server operation logs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,46 +1,60 @@
-import mongoose from 'mongoose';
-import app,{redisClient} from './app';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import app, { redisClient } from "./app";
+import dotenv from "dotenv";
 
 // To configure path to the environment variables
-dotenv.config({ path: 'config.env' });
+dotenv.config({ path: "config.env" });
 
 const PORT = process.env.PORT;
 
 // Database connection url
-const DB_URL = process.env.NODE_ENV === "production" ? process.env.DB_URL : process.env.DB_URL_DEV;
+const DB_URL =
+	process.env.NODE_ENV === "production"
+		? process.env.DB_URL
+		: process.env.DB_URL_DEV;
 
 // con is the connection object to the database
-mongoose.connect(DB_URL!).then((con) => {
-    if (con) console.log('Connection successfull');
-    else console.log('Connection failed');
-});
-redisClient.connect().then(() => {
-    console.log("redis connection successfull")
-})
+mongoose
+	.connect(DB_URL!)
+	.then((con) => {
+		if (con) console.log("SERVER : MongDB connection successfull");
+		else console.log("SERVER : MongoDB connection failed");
+	})
+	.catch((error) => console.log("SERVER (ERROR) :", error));
+
+redisClient
+	.connect()
+	.then(() => {
+		console.log("SERVER : Redis connection successfull");
+	})
+	.catch((error) => console.log("SERVER (ERROR) :", error));
 
 // start the server
 const server = app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+	console.log(`SERVER : Running at http://localhost:${PORT}`);
 });
 
 // For unhandled errors in synchronous code
-process.on('uncaughtException', (err) => {
-    console.log(err);
-    console.log('uncaught exception encountered!!! 💥...application crashed!');
-    server.close();
-    mongoose.disconnect();
+process.on("uncaughtException", (err) => {
+	console.log(err);
+	console.log(
+		"SERVER (ERROR) : uncaught exception encountered!!! 💥...application crashed!"
+	);
+	server.close();
+	mongoose.disconnect();
 });
 // For unhandled errors in async code
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    console.log('unhandled rejection encountered!!! 💥...application crashed!');
-    server.close();
-    mongoose.disconnect();
+process.on("unhandledRejection", (err) => {
+	console.log(err);
+	console.log(
+		"SERVER (ERROR) : unhandled rejection encountered!!! 💥...application crashed!"
+	);
+	server.close();
+	mongoose.disconnect();
 });
 // When hosting service closes our application it triggers SIGTERM event
-process.on('SIGTERM', () => {
-    console.log('SIGTERM recieved.... Closing server gracefully 😇');
-    server.close();
-    mongoose.disconnect();
+process.on("SIGTERM", () => {
+	console.log("SERVER : SIGTERM recieved.... Closing server gracefully 😇");
+	server.close();
+	mongoose.disconnect();
 });
