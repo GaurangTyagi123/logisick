0b06194bc12527d4dc1fbfaee8105fb25f8d22e4
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2025-11-08 19:08:50 +0530

    Update : access token to refresh + access tokens

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,34 +1,36 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
-	forgotPassword,
-	isLoggedIn,
-	login,
-	logout,
-	protect,
-	resetPassword,
-	signup,
-	verifyEmail,
-} from "../controllers/auth.controller";
+    forgotPassword,
+    isLoggedIn,
+    login,
+    logout,
+    protect,
+    refresh,
+    resetPassword,
+    signup,
+    verifyEmail,
+} from '../controllers/auth.controller';
 
 const authRouter = Router();
 
 // End-point to check whether the user is logged or not
-authRouter.get("/isLoggedIn", isLoggedIn);
+authRouter.get('/isLoggedIn', isLoggedIn);
 
 // End-point to sign up a new user
-authRouter.post("/signup", signup);
+authRouter.post('/signup', signup);
 
 // End-point to login
-authRouter.post("/login", login);
+authRouter.post('/login', login);
 
 // End-point to logout
-authRouter.get("/logout", protect, logout);
+authRouter.get('/logout', protect, logout);
 
+authRouter.post('/refresh', refresh);
 // End-points for resetting password if the user has forgotten it
-authRouter.post("/forgotPassword", forgotPassword);
-authRouter.patch("/resetPassword/:resetToken", resetPassword);
+authRouter.post('/forgotPassword', forgotPassword);
+authRouter.patch('/resetPassword/:resetToken', resetPassword);
 
 // End-point to verify user email
-authRouter.post("/verifyEmail", protect, verifyEmail);
+authRouter.post('/verifyEmail', protect, verifyEmail);
 
 export default authRouter;
