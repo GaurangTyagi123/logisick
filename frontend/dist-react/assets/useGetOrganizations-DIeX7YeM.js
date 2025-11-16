5c62b199a89b21fc8998412df3ac07184b0a40d0
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2025-11-16 08:41:38 +0530

    FIX : folder srestucture

--- frontend/dist-react/assets/useGetOrganizations-DIeX7YeM.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./apiOrg-BFNhkGiD.js";import{a as t}from"./index-CErwBKge.js";function o(){const{data:r,isPending:e}=t({queryKey:["orgs"],queryFn:s});return{data:r,isPending:e}}export{o as u};
