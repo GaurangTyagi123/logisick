0040c6e4c5fd79d141c754adc14189aae8f41712
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2025-11-28 19:30:52 +0530

    cleanup

--- frontend/dist-react/assets/useGetOrganizations-CSRPxPm0.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./apiOrg-BafY1MYC.js";import{b as n}from"./index-D2pWsFwA.js";function a(){const{data:r,isPending:t}=n({queryKey:["orgs"],queryFn:e});return{data:r,isGettingOrganizations:t}}export{a as u};
